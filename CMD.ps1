2ce832f990b68c58df513656463b82e3eb608dd1
Hanumant Patil <hapatil@microsoft.com>
2022-07-14 16:38:03 +0530

    Add people picker control solution

---
+++ CMD.ps1
@@ -0,0 +1,12 @@
+﻿## Create PCF Control
+pac pcf init --namespace PeoplePickerV1  --name PeoplePickerV1 --template field
+
+# Create Solution for PCF control
+pac solution init --publisher-name HP --publisher-prefix PCFPROD
+
+#Refer PCF control 
+pac solution add-reference --path "C:\TempData\ProjectCode\CVS\PCF Control\PeoplePickerV1"
+
+#Build PCF solution
+
+msbuild /t:build /restore
